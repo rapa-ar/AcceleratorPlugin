2428d9e9f65e2f13a6d523eaaefffe847a2ce48d
jpannellrapa <75494563+jpannellrapa@users.noreply.github.com>
2022-10-04 18:03:17 -0500

    nope

--- Plugin/Plugin.cpp
+++ Plugin/Plugin.cpp
@@ -332,11 +332,6 @@ void CreatePush(OrthancPluginRestOutput* output,
   result[KEY_ID] = id;
   result[KEY_PATH] = std::string(URI_PUSH) + "/" + id;
 
-  const char* const* headersKeys = request->headersKeys;
-  const char* const* headersValues = request->headersValues;
-  OrthancPluginSetHttpHeader(OrthancPlugins::GetGlobalContext(), output, "headersKeys", headersKeys.c_str());
-  OrthancPluginSetHttpHeader(OrthancPlugins::GetGlobalContext(), output, "headersValues", headersValues.c_str());
-
   std::string s = result.toStyledString();
   OrthancPluginSetHttpHeader(OrthancPlugins::GetGlobalContext(), output, "TransferID", id.c_str());
   OrthancPluginAnswerBuffer(OrthancPlugins::GetGlobalContext(), output, s.c_str(), s.size(), "application/json");
